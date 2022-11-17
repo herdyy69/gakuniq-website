d56e16f30bed1084bddc2fa768062b6caed7c1a5
herdyy69 <herdyansah203@gmail.com>
2022-11-17 13:19:08 +0700

    asa

--- src/pages/my-account/profiles.tsx
+++ src/pages/my-account/profiles.tsx
@@ -596,65 +596,75 @@ const MyProfiles = () => {
                             className="modal-toggle"
                           />
                           <div className="modal">
-                            <form
-                              onSubmit={() => {
-                                axios
-                                  .post(
-                                    `/api/refund_produk/create/${item.id}`,
-                                    {
-                                      alasan: alasans,
-                                      transaksi_id: item.id,
-                                    }
-                                  )
-                                  .then((res) => {
-                                    setTimeout(() => {
-                                      router.reload();
-                                    }, 1000);
-                                  })
-                                  .catch((err) => {
-                                    console.log(err);
-                                  });
-                              }}
-                            >
-                              <div className="modal-box relative bg-slate-200 w-full">
+                            {/* <form> */}
+                            <div className="modal-box relative bg-slate-200 w-full">
+                              <label
+                                htmlFor="my-modal-refund"
+                                className="btn btn-sm btn-circle absolute right-2 top-2 glass"
+                              >
+                                ✕
+                              </label>
+                              <h3 className="text-lg font-bold">
+                                Refund Produk
+                              </h3>
+                              <span className="text-sm text-gray-500">
+                                Silakan isi alasan untuk refund produk
+                              </span>
+                              <div className="flex flex-row items-center">
+                                <input
+                                  value={alasans}
+                                  onChange={(e) => setAlasans(e.target.value)}
+                                  type="text"
+                                  className="input input-bordered input-sm bg-slate-50 w-full"
+                                  placeholder="Ketik alasan..."
+                                />
+                              </div>
+
+                              <div className="flex flex-row items-center mt-3">
                                 <label
                                   htmlFor="my-modal-refund"
-                                  className="btn btn-sm btn-circle absolute right-2 top-2 glass"
+                                  className="btn bg-red-500 text-white border-none btn-sm rounded-none"
                                 >
-                                  ✕
+                                  Batal
                                 </label>
-                                <h3 className="text-lg font-bold">
-                                  Refund Produk
-                                </h3>
-                                <span className="text-sm text-gray-500">
-                                  Silakan isi alasan untuk refund produk
-                                </span>
-                                <div className="flex flex-row items-center">
-                                  <input
-                                    value={alasans}
-                                    onChange={(e) => setAlasans(e.target.value)}
-                                    type="text"
-                                    className="input input-bordered input-sm bg-slate-50 w-full"
-                                    placeholder="Ketik alasan..."
-                                  />
-                                </div>
-
-                                <div className="flex flex-row items-center mt-3">
-                                  <label
-                                    htmlFor="my-modal-refund"
-                                    className="btn bg-red-500 text-white border-none btn-sm rounded-none"
-                                  >
-                                    Batal
-                                  </label>
-                                  <button
-                                    type="submit"
-                                    className="btn btn-primary btn-sm rounded-none"
-                                  >
-                                    Konfirmasi
-                                  </button>
-                                </div>
+                                <button
+                                  onClick={() => {
+                                    axios
+                                      .post(
+                                        `/api/refund_produk/create/${item.id}`,
+                                        {
+                                          alasan: alasans,
+                                          transaksi_id: item.id,
+                                        }
+                                      )
+                                      .then((res) => {
+                                        axios
+                                          .put(
+                                            `/api/history/${item?.id}/edit`,
+                                            {
+                                              status: 'pengajuan refund',
+                                            }
+                                          )
+                                          .then((res) => {
+                                            setTimeout(() => {
+                                              router.reload();
+                                            }, 1000);
+                                          })
+                                          .catch((err) => {
+                                            console.log(err);
+                                          });
+                                      })
+                                      .catch((err) => {
+                                        console.log(err);
+                                      });
+                                  }}
+                                  className="btn btn-primary btn-sm rounded-none"
+                                >
+                                  Konfirmasi
+                                </button>
                               </div>
-                            </form>
+                            </div>
+                            {/* </form> */}
                           </div>
                           <label
                             className="btn btn-ghost bg-red-500 text-sm btn-sm font-bold rounded-none p-2 mx-1 text-white"
